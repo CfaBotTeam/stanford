0b736314d782f07f6f8cff6040ae6cae12215c32
biarne-a <biarnes.adrien@gmail.com>
2018-03-06 11:13:38 +0100

    crawling investopedia cfa

---
+++ crawling/merge_docs.py
@@ -0,0 +1,14 @@
+import json
+
+if __name__ == '__main__':
+    with open('investopedia_cfa.json', 'r') as f_invest, \
+         open('data_collected.json', 'r') as f_docs:
+        invest_red = f_invest.read()
+        json_invest = json.loads(invest_red)
+        docs_red = f_docs.read()
+        json_docs = json.loads(docs_red)
+        json_invest['docs'] = json_invest['docs'] + json_docs
+
+    with open('all_data_collected.json', 'w') as f_docs:
+        to_write = json.dumps(json_invest)
+        f_docs.write(to_write)
