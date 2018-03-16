959005c08317676cf610af75d4cdcd04e2730da0
biarne-a <biarnes.adrien@gmail.com>
2018-03-16 12:30:25 +0100

    Anotate questions

--- crawling/merge_docs.py
+++ crawling/merge_docs.py
@@ -1,6 +1,7 @@
 import json
 
 if __name__ == '__main__':
+    all_data = []
     with open('investopedia_cfa.json', 'r') as f_invest, \
          open('data_collected.json', 'r') as f_docs:
         invest_red = f_invest.read()
@@ -9,6 +10,7 @@ if __name__ == '__main__':
         json_docs = json.loads(docs_red)
         all_docs = json_invest['docs'] + json_docs
 
-    with open('all_data_collected.json', 'w') as f_docs:
-        to_write = json.dumps(all_docs)
-        f_docs.write(to_write)
+    with open('all_data_collected.txt', 'w') as f_docs:
+        for doc in all_docs:
+            to_write = json.dumps(doc)
+            f_docs.write(to_write + '\n')
